324f74584925ccc9e035394d14c43b9f38884eb9
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-11-18 14:37:24 +0600

    fixed super user

--- aql_user/managers.py
+++ aql_user/managers.py
@@ -13,6 +13,7 @@ class UserManager(BaseUserManager):
         user=self.create_user(email=email, password=password)
         user.is_admin=True
         user.is_staff=True
+        user.is_active=True
         user.is_superuser=True
         user.save(using=self._db)
         return user
